0a5787a9507c192bce8de541ec4b9e11b5fd8eca
ugiravi <158242138+ugiravi@users.noreply.github.com>
2024-06-06 13:01:25 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,3 +18,10 @@ CREATE TABLE comments (
   user_id INTEGER REFERENCES users(id),
   comment TEXT
 );
+
+
+-- Retrieve posts by a specific user
+SELECT * FROM posts WHERE user_id = $1;
+
+-- Count comments on a post
+SELECT COUNT(*) FROM comments WHERE post_id = $1;
